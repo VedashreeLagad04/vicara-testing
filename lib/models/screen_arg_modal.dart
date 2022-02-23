325b10f7d11df5248ebb5f77cfa1dc4457bd8004
VedashreeLagad04 <81160051+VedashreeLagad04@users.noreply.github.com>
2022-02-23 12:21:12 +0530

    dhiraj's changes in home_screen.dart

--- lib/models/screen_arg_modal.dart
+++
@@ -1,8 +0,0 @@
-class ScreenArgs {
-  late Map<String, dynamic>? screenArgs;
-  ScreenArgs();
-  ScreenArgs.fromMAP(Map<String, dynamic> data) {
-    screenArgs = data;
-  }
-  Map<String, dynamic>? get getArgs => screenArgs;
-}
